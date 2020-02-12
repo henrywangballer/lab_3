6f70933b0b70539ac300d6b1b6543da9efc09655
henrywangballer <hewa1459@colorado.edu>
2020-02-11 23:21:19 -0700

    lab3_script.sh file

---
+++ lab3_script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Authors : Henry
+# Date: 2/5/2020
+
+#Problem 1 Code:
+#Make sure to document how you are solving each problem!
+
+#echo "Enter a filename: "
+#read fileName
+#echo "Enter a regular expression: "
+#read regEx
+
+#grep $regEx $fileName
+
+#grep -c '^\(([0-9]\{3\})\|[0-9]\{3\}\)[-]\?[0-9]\{3\}[-]\?[0-9]\{4\}$' regex_practice.txt
+
+grep -c '[A-Za-z0-9._]@[A-Za-z0-9.][A-Za-z]\{2,\}' regex_practice.txt
+
+grep -o '^303[-]\?[0-9]\{3\}[-]\?[0-9]\{4\}$' regex_practice.txt
+
+grep '[A-Za-z0-9._]@geocities.com' regex_practice.txt >> email_results.txt
+
