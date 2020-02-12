4a295ebb63bf1bbbb54cd25b123ea1715de4be54
henrywangballer <hewa1459@colorado.edu>
2020-02-11 23:27:23 -0700

    lab_3_link.txt Adding all of the files

--- lab3_script.sh
+++ lab3_script.sh
@@ -5,14 +5,14 @@
 #Problem 1 Code:
 #Make sure to document how you are solving each problem!
 
-#echo "Enter a filename: "
-#read fileName
-#echo "Enter a regular expression: "
-#read regEx
+echo "Enter a filename: "
+read fileName
+echo "Enter a regular expression: "
+read regEx
 
-#grep $regEx $fileName
+grep $regEx $fileName
 
-#grep -c '^\(([0-9]\{3\})\|[0-9]\{3\}\)[-]\?[0-9]\{3\}[-]\?[0-9]\{4\}$' regex_practice.txt
+grep -c '^\(([0-9]\{3\})\|[0-9]\{3\}\)[-]\?[0-9]\{3\}[-]\?[0-9]\{4\}$' regex_practice.txt
 
 grep -c '[A-Za-z0-9._]@[A-Za-z0-9.][A-Za-z]\{2,\}' regex_practice.txt
 
